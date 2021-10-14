fd22e5d87e345b2639407deea6de19b45b3b4af7
lecocococo <chemwang13@naver.com>
2021-10-15 03:52:47 +0900

    Refactor: 미사용 코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,31 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 
-let board = [
-  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-  [".", "9", "8", ".", ".", ".", ".", "6", "."],
-  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-  [".", "6", ".", ".", ".", ".", "2", "8", "."],
-  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
-];
-function makeBoard() {
-  let num = 1;
-  let table = "<table>";
-
-  for (let i = 0; i < 10; i++) {
-    table += "<tr>";
-    for (let j = 0; j < 9; j++) {
-      table += "<td>" + num + "</td>";
-    }
-    table += "</tr>";
-  }
-  table += "</table>";
-  return table;
-}
 // subject라는 이름의 태그를 만든다
 class Subject extends Component {
   render() {
@@ -39,18 +14,6 @@ class Subject extends Component {
 }
 class Map extends Component {
   render() {
-    //     let num = 1;
-    //     let table = "<table>";
-
-    //     for (let i = 0; i < 10; i++) {
-    //       table += "<tr>";
-    //       for (let j = 0; j < 9; j++) {
-    //         table += "<td>" + num + "</td>";
-    //       }
-    //       table += "</tr>";
-    //     }
-    //     table += "</table>";
-    //     return { table };
     let board = [
       ["5", "3", ".", ".", "7", ".", ".", ".", "."],
       ["6", ".", ".", "1", "9", "5", ".", ".", "."],
