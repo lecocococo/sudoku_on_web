590e083044b032efe53b122d496ed3a204704708
lecocococo <chemwang13@naver.com>
2021-10-16 04:18:23 +0900

    Feat:입력 가능한 테이블로 바꿈/ 버튼추가/ 스도쿠 규칙 추가

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class Subject extends Component {
     return (
       <header>
         <h1>SUDOKU</h1>
-        Let's play! lol
+        <p>Let's play! lol</p>
       </header>
     );
   }
@@ -28,21 +28,61 @@ class Map extends Component {
     const table = board.map((n) => (
       <tr>
         {n.map((m) => (
-          <td>{m}</td>
+          <td>
+            {/* readOnly는 읽기만 가능할뿐 값변경은 불가능 */}
+            {/* disabled도 읽기만 가능할뿐 값변경은 불가능 + form으로 전송시 값 전송 안됨 */}
+            {/* 삼항연산자 이용해 . 일 경우에만 값을 입력할 수 있도록함 */}
+            {m === "." ? (
+              <input type="text" />
+            ) : (
+              <input type="text" value={m} readOnly />
+            )}
+          </td>
         ))}
       </tr>
     ));
-    return <table>{table}</table>;
+    return (
+      <div>
+        <table id="table">{table}</table>
+      </div>
+    );
   }
 }
 
+class Rule extends Component {
+  render() {
+    return (
+      <div id="explain">
+        <h2>Rule:</h2>
+        <p> 1.Use Numbers 1-9</p>
+        <p>
+          2.Don’t Repeat Any Numbers within the row, column or square.(square
+          means 3 x 3 spaces)
+        </p>
+      </div>
+    );
+  }
+}
+class Button extends Component {
+  render() {
+    return <button type="button">Click here when you Finish</button>;
+  }
+}
 function App() {
   return (
     <div className="App">
       <Subject></Subject>
       <Map></Map>
+      <Button></Button>
+      <Rule></Rule>
     </div>
   );
 }
 
 export default App;
+
+// 참조
+// 깃 커밋 메세지 컨벤션: https://richone.tistory.com/26
+// 리액트 if문 사용법:https://rios.tistory.com/entry/React-%EB%A6%AC%EC%95%A1%ED%8A%B8-IF-%EB%AC%B8-%EC%82%AC%EC%9A%A9%EB%B2%95
+// 스도쿠 판:https://codepen.io/CYass/pen/qwjbRr
+// css id,class: https://yjshin.tistory.com/entry/HTML5-CSS3-7-CSS3-id-class-%EC%86%8D%EC%84%B1%ED%9B%84%EC%86%90%EC%9E%90%EC%86%90%EB%8F%99%EC%9C%84-%EC%84%A0%ED%83%9D
