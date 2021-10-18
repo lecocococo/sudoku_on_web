4802d56fbf44fc89ce3e60d141d79684227bfdb0
lecocococo <chemwang13@naver.com>
2021-10-18 21:29:50 +0900

    Refactor: 수정 스도쿠판 테이블과 버튼                                                                        스도쿠판 내부의 입력받는  input과  버튼을 교체(외부js파이로부터 가져옴)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+// 할것
+// input에 값을 입력했을때 그 값을 스도쿠 배열에 채워두게한다
+// 그리고 정답과 맞는지 비교하게 만든다
 import React, { Component } from "react";
+import { useState } from "react";
 import "./App.css";
 import * as sudokuMaker from "./make-sudoku-board";
+import Inputs from "./Inputs";
+import CheckAnswer from "./check-answer";
+
+const b = sudokuMaker.newSudokuBoard(5);
+sessionStorage.setItem("solvedBoard", b[1]);
 
 // subject라는 이름의 태그를 만든다
 class Subject extends Component {
@@ -15,35 +24,26 @@ class Subject extends Component {
 }
 class Map extends Component {
   render() {
-    // let board = [
-    //   ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-    //   ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-    //   [".", "9", "8", ".", ".", ".", ".", "6", "."],
-    //   ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-    //   ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-    //   ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-    //   [".", "6", ".", ".", ".", ".", "2", "8", "."],
-    //   [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-    //   [".", ".", ".", ".", "8", ".", ".", "7", "9"],
-    // ];
-    let b = sudokuMaker.newSudokuBoard(5)[1];
-    console.log(b);
-    const table = b.map((n) => (
-      <tr>
-        {n.map((m) => (
-          <td>
-            {/* readOnly는 읽기만 가능할뿐 값변경은 불가능 */}
-            {/* disabled도 읽기만 가능할뿐 값변경은 불가능 + form으로 전송시 값 전송 안됨 */}
-            {/* 삼항연산자 이용해 . 일 경우에만 값을 입력할 수 있도록함 */}
-            {m === 0 ? (
-              <input type="text" />
-            ) : (
-              <input type="text" value={m} readOnly />
-            )}
-          </td>
-        ))}
-      </tr>
+    // console.log(b);
+    const table = b[0].map((n) => (
+      <tbody>
+        <tr>
+          {n.map((m) => (
+            <td>
+              {/* readOnly는 읽기만 가능할뿐 값변경은 불가능 */}
+              {/* disabled도 읽기만 가능할뿐 값변경은 불가능 + form으로 전송시 값 전송 안됨 */}
+              {/* 삼항연산자 이용해 . 일 경우에만 값을 입력할 수 있도록함 */}
+              {m === 0 ? (
+                <Inputs />
+              ) : (
+                <input type="number" value={m} readOnly />
+              )}
+            </td>
+          ))}
+        </tr>
+      </tbody>
     ));
+
     return (
       <div>
         <table id="table">{table}</table>
@@ -57,9 +57,9 @@ class Rule extends Component {
     return (
       <div id="explain">
         <h2>Rule:</h2>
-        <p> 1.Use Numbers 1-9</p>
+        <p> 1. Use Numbers 1-9</p>
         <p>
-          2.Don’t Repeat Any Numbers within the row, column or square.(square
+          2. Don’t Repeat Any Numbers within the row, column or square.(square
           means 3 x 3 spaces)
         </p>
       </div>
@@ -68,7 +68,7 @@ class Rule extends Component {
 }
 class Button extends Component {
   render() {
-    return <button type="button">Click here when you Finish</button>;
+    return <CheckAnswer />;
   }
 }
 function App() {
@@ -77,9 +77,16 @@ function App() {
       <Subject></Subject>
       <Map></Map>
       <Button></Button>
+      {/* <button id="check" type="button">
+        Click here when you Finish
+      </button> */}
       <Rule></Rule>
+      <Inputs />
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </div>
   );
+  // b = null;
 }
 
 export default App;
@@ -91,3 +98,5 @@ export default App;
 // css id,class: https://yjshin.tistory.com/entry/HTML5-CSS3-7-CSS3-id-class-%EC%86%8D%EC%84%B1%ED%9B%84%EC%86%90%EC%9E%90%EC%86%90%EB%8F%99%EC%9C%84-%EC%84%A0%ED%83%9D
 // js 스프레드 오퍼레이터(...):https://paperblock.tistory.com/62
 // js 배열 메서드: https://offbyone.tistory.com/133
+// react 이벤트 처리하기:https://ko.reactjs.org/docs/handling-events.html
+// 자바스크립트 배열 초기화(길이를 0으로):https://jamesdreaming.tistory.com/44
