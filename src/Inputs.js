1a88963aa59da76b39db2f4f5fa494cc6597901a
lecocococo <chemwang13@naver.com>
2021-10-18 21:46:10 +0900

    Feat: 추가 스도쿠판에서 빈셀의 input에 입력 값제한, onChange, onKeyUp 함수 추가

---
+++ src/Inputs.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+function Inputs() {
+  const [text, setText] = useState("");
+  // 값 변경 시 이벤트 객체가 파라미터(e)에 담김
+  const onChange = (e) => {
+    // e.target에는 이벤트가 발생한 input DOM에 대한 정보를 가지고 있음
+    // console.log(e.target);
+    // 이벤트가 발생한 DOM의 값 가져오기
+    // console.log(e.target.value);
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        onChange={onChange}
+        type="text"
+        maxLength="1"
+        onKeyUp={(e) => {
+          const { value } = e.target;
+          // value의 값이 숫자가 아닐경우 빈문자열로 replace
+          const isNumber = value.replace(/[^0-9]/g, "");
+          //backspace의 keyCode값은 8, event의 keycode가 8이면 alert안해주기 위한 코드
+          if (isNumber === "" && e.keyCode !== 8) {
+            alert("1~9사이의 정수를 입력하세요");
+          }
+        }}
+        value={text}
+      />
+    </div>
+  );
+}
+
+export default Inputs;
